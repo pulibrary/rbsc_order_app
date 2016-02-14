8caa182d722f68a3aa50c9c38ad10585f86077bd
Kevin Reiss <kevin.reiss@gmail.com>
2016-02-14 01:27:10 -0600

    High Voltage for Static Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'high_voltage/pages#show', id: 'about'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
